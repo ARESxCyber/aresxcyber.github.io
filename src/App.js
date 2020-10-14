ea154556635e0e263c87413b1cbc832e0caf3b99
lx0 <lx0@ares>
2020-10-14 11:23:22 -0300

    Add basic content to page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Title>ARESx</Title>
+      <Rank>Current Rank: X</Rank>
+      <Navigation>
+        <li><a href="#">Write Ups</a></li>
+        <li><a href="#">Members</a></li>
+        <li><a href="#">Scores</a></li>
+      </Navigation>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+  color: red;
+`;
+
+const Title = styled.h1`
+  margin: 0;
+`;
+
+const Rank = styled.h2`
+  margin: 20px 0 0 0;
+  text-transform: uppercase;
+  color: #af0000;
+`;
+
+const Navigation = styled.ul`
+  display: flex;
+  text-transform: uppercase;
+  list-style: none;
+  margin-top: 60px;
+  padding: 0;
+
+  li {
+    margin: 0 20px;
+  }
+
+  a {
+    color: inherit;
+    font-size: 30px;
+    text-decoration: none;
+    transition: color 0.2s ease-in-out;
+  }
+
+  a:hover {
+    color: #af0000;
+  }
+`;
 
 export default App;
