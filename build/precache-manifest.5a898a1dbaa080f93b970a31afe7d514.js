6d7cc62b192e65ff3498db39ca1b7226b90c1ce4
lx0 <lx0@ares>
2020-10-14 10:53:32 -0300

    Add production build to repo for gh-pages

---
+++ build/precache-manifest.5a898a1dbaa080f93b970a31afe7d514.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7979d6e5aab589ebd97a4e142b7a65f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d9afe671f1861627975",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a0863aeea2c748814209",
+    "url": "/static/js/2.663f1559.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.663f1559.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d9afe671f1861627975",
+    "url": "/static/js/main.04c0e66b.chunk.js"
+  },
+  {
+    "revision": "a0e3be69f90b84d2f18f",
+    "url": "/static/js/runtime-main.05742c74.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
