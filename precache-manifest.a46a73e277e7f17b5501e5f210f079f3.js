989f1cd4aef151b75c5c6d97586bac7324ba1ba5
EnigmaEnvoy <suma.thota98@gmail.com>
2020-10-15 15:43:51 +0530

    Updates

---
+++ precache-manifest.a46a73e277e7f17b5501e5f210f079f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "139627cc594bef1c94dfcb1539008e49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1564e4bcec1a6463f131",
+    "url": "/static/css/main.0342c391.chunk.css"
+  },
+  {
+    "revision": "f2a27487d7158873c706",
+    "url": "/static/js/2.f200a7d9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f200a7d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1564e4bcec1a6463f131",
+    "url": "/static/js/main.dcaea75f.chunk.js"
+  },
+  {
+    "revision": "a0e3be69f90b84d2f18f",
+    "url": "/static/js/runtime-main.05742c74.js"
+  },
+  {
+    "revision": "fbb3dd03428fafc7115f4b521cee063f",
+    "url": "/static/media/ARESx_Logo.fbb3dd03.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.351f15a785a1800ff7e1cadb2ab95f3f.js"
+  "/precache-manifest.a46a73e277e7f17b5501e5f210f079f3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
