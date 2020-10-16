07a08bccaccd38f7bcf0c76bb8c0bbdfe65a4703
EnigmaEnvoy <suma.thota98@gmail.com>
2020-10-16 16:19:21 +0530

    Updates

--- precache-manifest.2e7467de4ca2458e7c1fd0feb03fe5e0.js
+++ precache-manifest.2e7467de4ca2458e7c1fd0feb03fe5e0.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8b564b3f71c5f5e006896cf753710b05",
+    "revision": "61db156f1b81d9b68fffe4658fe486f5",
     "url": "/index.html"
   },
   {
-    "revision": "c37a7297b9800d438121",
+    "revision": "9620a25ad96efcaef52a",
     "url": "/static/css/main.0342c391.chunk.css"
   },
   {
-    "revision": "f2a27487d7158873c706",
-    "url": "/static/js/2.f200a7d9.chunk.js"
+    "revision": "728305f7749aac2f6e28",
+    "url": "/static/js/2.cfdfc3ed.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/static/js/2.f200a7d9.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.cfdfc3ed.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c37a7297b9800d438121",
-    "url": "/static/js/main.80b9630c.chunk.js"
+    "revision": "9620a25ad96efcaef52a",
+    "url": "/static/js/main.29d3e41c.chunk.js"
   },
   {
     "revision": "a0e3be69f90b84d2f18f",
